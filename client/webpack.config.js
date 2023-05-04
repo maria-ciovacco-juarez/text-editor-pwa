a1ab134643a6a6db3ed371e347f4a668e94a9bed
Maria Ciovacco Juarez <mariaciovaccojuarez@MBP-Maria.lan>
2023-05-04 17:23:33 -0500

    added webpacks and scripts

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,12 +18,52 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Jate',
+      }),
+
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        name: 'Text Editor',
+        short_name: 'TE',
+        description: 'This is a application to take notes.',
+        background_color: '#272822',
+        fingerprints: false,
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons')
+
+          }
+        ]
+      })
     ],
 
     module: {
       rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
         
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
