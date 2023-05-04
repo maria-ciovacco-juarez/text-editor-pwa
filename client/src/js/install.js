cc361d623a150fa30bf4a7f20a31115ba48493be
Maria Ciovacco Juarez <mariaciovaccojuarez@MBP-Maria.lan>
2023-05-04 17:43:01 -0500

    prompts for install

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -9,7 +9,33 @@ window.addEventListener('beforeinstallprompt', (event) => {
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+  const promptEvent = window.deferredPrompt;
+
+  if (!promptEvent) {
+    return;
+  }
+
+  // Show prompt
+  promptEvent.prompt();
+
+  // Wait for the user to respond to the prompt
+  const { outcome } = await promptEvent.userChoice;
+
+  // Reset the deferred prompt variable, it can only be used once.
+  window.deferredPrompt = null;
+
+  // Hide the install button if the user chooses to install
+  if (outcome === 'accepted') {
+    butInstall.classList.add('hidden');
+  }
+});
+
+
+  // Hide the install button
+  butInstall.classList.add('hidden');
+});
+
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {});
